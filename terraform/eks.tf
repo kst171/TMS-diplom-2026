680d59f6a374739c2db6f09b15e599a7b055a9c1
Kiryl Tymanovich <tymanovich17@gmail.com>
2026-06-05 14:16:21 +0300

    security fix

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,5 +1,3 @@
-# eks.tf
-
 module "eks" {
   source = "./modules/eks"
 
@@ -7,21 +5,14 @@ module "eks" {
   kubernetes_version = "1.31" 
 
   endpoint_public_access = true
-  
-  # Интеграция прав администратора для вашей учетной записи
   enable_cluster_creator_admin_permissions = true
-
-  # Авторизация в гибридный режим, чтобы ноды могли пройти аутентификацию
   authentication_mode = "API_AND_CONFIG_MAP"
-
-  # Включает механизм доверия OIDC/IRSA для авторизации подов
   enable_irsa = true
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # Встроенные сетевые аддоны (используем вашу структуру переменных)
   addons = {
     coredns = {
       most_recent = true
@@ -31,7 +22,7 @@ module "eks" {
     }
     vpc-cni = {
       most_recent    = true
-      before_compute = true # Критично для правильной инициализации сети нод
+      before_compute = true
     }
     eks-pod-identity-agent = {
       most_recent = true
@@ -41,13 +32,12 @@ module "eks" {
   eks_managed_node_groups = {
     main = {
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 2
+      desired_size = 1
 
       instance_types = ["c7i-flex.large"] 
       capacity_type  = "ON_DEMAND"        
 
-      # Явно разрешаем AWS автоматически подключить ноды к Access Entries
       create_access_entry = true
 
       iam_role_additional_policies = {
@@ -64,7 +54,6 @@ module "eks" {
   }
 }
 
-# Автоматическое прописывание раннера Actions в белый список Kubernetes
 resource "kubernetes_config_map_v1_data" "aws_auth" {
   force = true
   metadata {
@@ -73,12 +62,10 @@ resource "kubernetes_config_map_v1_data" "aws_auth" {
   }
 
   data = {
-    # Исправлено: жестко прописываем пустой mapRoles, так как в EKS 1.31 
-    # управляемые ноды регистрируются через Access Entries автоматически
     mapRoles = "[]"
     
     mapUsers = <<YAML
-- userarn: arn:aws:iam::372898429717:user/github-actions-user
+- userarn: arn:aws:iam::${var.aws_account_id}:user/github-actions-user
   username: github-actions-user
   groups:
     - system:masters
@@ -88,8 +75,6 @@ YAML
   depends_on = [module.eks]
 }
 
-# Автоматическое создание привязки прав ClusterAdmin на полный ARN
-# Исправлено: использован современный ресурс v1 для исключения Warning
 resource "kubernetes_cluster_role_binding_v1" "github_actions_arn_binding" {
   metadata {
     name = "github-actions-arn-binding"
@@ -103,7 +88,7 @@ resource "kubernetes_cluster_role_binding_v1" "github_actions_arn_binding" {
 
   subject {
     kind      = "User"
-    name      = "arn:aws:iam::372898429717:user/github-actions-user"
+    name      = "arn:aws:iam::${var.aws_account_id}:user/github-actions-user"
     api_group = "rbac.authorization.k8s.io"
   }
 
